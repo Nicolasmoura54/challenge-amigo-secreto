689842f5b1becf92157383ee35b8ba98a50ec983
Nicolas Moura <nicolasmouraignacio@outlook.com>
2025-08-07 18:06:40 -0300

    Função adicionarAmigo inserida

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+  let nomeDoAmigo = document.getElementById("amigo").value;
+  if (nomeDoAmigo == " ") {
+    alert("Por favor, insira um nome");
+    return;
+  }
+  amigos.push(nomeDoAmigo);
+  document.getElementById("amigo").value = "";
+}
